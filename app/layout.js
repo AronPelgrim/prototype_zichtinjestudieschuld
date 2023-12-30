0592f677598d166a7ea1432eb9f2940ab6918f99
AronPelgrim <pelgrim.aron@gmail.com>
2023-12-30 19:11:16 +0100

    Result taking shape, studieschuld done

--- app/layout.js
+++ app/layout.js
@@ -12,6 +12,16 @@ export default function RootLayout({ children }) {
           rel="stylesheet"
           href="https://use.typekit.net/blx5jvp.css"
         ></link>
+        <link rel="preconnect" href="https://fonts.googleapis.com"></link>
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossorigin
+        ></link>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Montserrat:wght@500&display=swap"
+          rel="stylesheet"
+        ></link>
       </head>
       <body>{children}</body>
     </html>
