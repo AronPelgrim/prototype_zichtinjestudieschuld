b7fda0ee1eafc881016f3417117619fa35072446
AronPelgrim <pelgrim.aron@gmail.com>
2024-01-11 14:39:35 +0100

    organised tm vorige-volgende

--- app/components/backg7.js
+++ app/components/backg7.js
@@ -11,7 +11,7 @@ const Backg7 = () => {
         xmlns="http://www.w3.org/2000/svg"
         xmlnsXlink="http://www.w3.org/1999/xlink"
         viewBox="0 0 1344 767.469"
-        className="background"
+        className="background-svg"
       >
         <defs>
           <linearGradient
