a7bf9bd7291b1e1d98bf9a2420e607a758897145
AronPelgrim <pelgrim.aron@gmail.com>
2024-01-11 21:16:20 +0100

    Comments done page1

--- app/page.js
+++ app/page.js
@@ -6,45 +6,51 @@ import "../styles/Global.css";
 import Logo from "./components/logo";
 
 const Onboarding1 = () => {
+  // State voor het bijhouden van schermoriëntatie, getoonde tekst en antwoordstatus
   const [orientation, setOrientation] = useState("");
-
   const [displayedText, setDisplayedText] = useState("");
   const [antwoord, setAntwoord] = useState(false);
 
+  // Effect om schermoriëntatie te controleren en bij te werken
   useEffect(() => {
     const handleOrientationChange = () => {
+      // Controleer of de huidige oriëntatie staand (portrait) of liggend (landscape) is
       const isPortrait = window.matchMedia("(orientation: portrait)").matches;
 
-      if (isPortrait) {
-        setOrientation("Portrait");
-      } else {
-        setOrientation("Landscape");
-      }
+      // Zet de oriëntatie state op basis van de schermoriëntatie
+      setOrientation(isPortrait ? "Portrait" : "Landscape");
     };
 
+    // Roep de oriëntatiefunctie aan bij het laden en luister naar wijzigingen
     handleOrientationChange();
-
     window.addEventListener("resize", handleOrientationChange);
 
+    // Voorkom geheugenlekken door luisteraar te verwijderen bij het opruimen
     return () => {
       window.removeEventListener("resize", handleOrientationChange);
     };
   }, []);
 
+  // Effect voor het geleidelijk weergeven van een begroetingstekst
   useEffect(() => {
+    // Begroetingstekst instellen
     const headerText = `Goeiedag, ik ben 
-    Frans Ferdinand de derde, expert in studieschulden.`;
+  Frans Ferdinand de derde, expert in studieschulden.`;
 
+    // Variabele en interval voor het geleidelijk updaten van de getoonde tekst
     let index = 0;
     const interval = setInterval(() => {
       setDisplayedText(headerText.substring(0, index));
       index++;
+
+      // Stop het interval en geef antwoordstatus als de tekst compleet is
       if (index > headerText.length) {
         clearInterval(interval);
         setAntwoord(true);
       }
     }, 10);
 
+    // Voorkom geheugenlekken door interval te wissen bij het opruimen
     return () => clearInterval(interval);
   }, []);
 
